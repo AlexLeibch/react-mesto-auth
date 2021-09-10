e80dee5e66becfe0c27ac057cc5e5517d893f3e9
Alexanrd Leibch <71503887+DeadlyVi@users.noreply.github.com>
2021-09-10 17:33:37 +0200

    send to first rewiev

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import '../index.css';
 import React from 'react';
 
+import registrationAccept from '../vendor/image/AcceptRegistration.svg'
+import registrationReject from '../vendor/image/RejectRegistration.svg'
+
 import Header  from './Header';
 import Footer from './Footer';
 import Main from './Main';
@@ -15,9 +18,13 @@ import AddPlacePopup from './AddPlacePopup';
 import Login from './Login'
 import Register from './Register'
 import ProtectedRoute from './ProtectedRoute';
-import {Switch, Redirect, Route} from 'react-router-dom'
+import {Switch, Redirect, Route, useHistory} from 'react-router-dom'
+import InfoTooltip from './InfoTooltip';
+import * as auth from '../utils/auth'
 
 function App() {
+
+    const history = useHistory()
 
     const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false)
     const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false)
@@ -25,7 +32,12 @@ function App() {
     const [selectedCard, setSelectedCards] = React.useState({isOpen: false})
     const [currentUser, setCurrentUser] = React.useState('')
     const [cards, setCards] = React.useState([])
+    const [isToooltipOpen, setIsTooltipOpen] = React.useState(false)
+
     const [loggedIn, setLoggedIn] = React.useState(false)
+    const [message, setMessage] = React.useState({imgPath: '', text: ''})
+    const [email, setEmail] = React.useState('')
+
 
     React.useEffect(()  => {
         api.getInitialCards()
@@ -69,6 +81,19 @@ function App() {
         })
     }, [])  
 
+    React.useEffect(() => {
+        const jwt = localStorage.getItem('jwt')
+        if(jwt) {
+            auth.getContent(jwt)
+                .then((res) => {
+                    setLoggedIn(true)
+                    setEmail(res.data.email)
+                    history.push('/')
+                })
+                .catch((err) => console.log(err))
+        }
+    }, [history])
+
 
 
     function handleEditAvatarClick () {
@@ -92,6 +117,7 @@ function App() {
         setIsEditAvatarPopupOpen(false)
         setIsAddPlacePopupOpen(false)
         setSelectedCards({isOpen: false})
+        setIsTooltipOpen(false)
     }
 
     function handleUpdateUser({name, about}) {
@@ -131,7 +157,56 @@ function App() {
     }
 
     function handleSignOut() {
-        setLoggedIn(true)
+        setLoggedIn(false);
+        localStorage.removeItem('jwt');
+        setEmail('')
+        history.push('/sign-in')
+    }
+
+
+
+    function handleToolltipInfoOpen() {
+        setIsTooltipOpen(true)
+    }
+
+    function registration({email, password}) {
+        auth.register(email, password)
+        .then((res) => {
+            if (res.status === 201) {
+                handleTooltipInfo({imgPath: registrationAccept, text: 'Вы успешно зарегестрированы'})
+                handleToolltipInfoOpen()
+                history.push('/sign-in')
+            }
+            })
+        .catch((err) => {
+            handleTooltipInfo({imgPath: registrationReject, text: 'Что - то пошло не так'})
+            handleToolltipInfoOpen()
+            console.log(err)
+        })
+    }
+
+    function authorization({email, password}) {
+        auth.authorize({email, password})
+        .then((data) => {
+            if (!data) {
+                return
+            }
+        setLoggedIn(true);
+        handleTooltipInfo({imgPath: registrationAccept, text: 'Вы успешно авторизированы'});
+        history.push('/')
+        handleToolltipInfoOpen()
+
+        }).catch((err) => {
+            handleTooltipInfo({imgPath: registrationReject, text: 'Что - то пошло не так'})
+            handleToolltipInfoOpen()
+
+            console.log(err)
+
+        })
+    };
+
+    function handleTooltipInfo({imgPath, text}) {
+        setMessage({imgPath, text})
     }
 
   return (
@@ -141,13 +216,14 @@ function App() {
             <Header handleSignOut={handleSignOut}/>
                 <Switch>
                     <Route path="/sign-in">
-                        <Login/>
+                        <Login authorization={authorization}/>
                     </Route>
                     <Route path="/sign-up">
-                        <Register/>
+                        <Register registration={registration}/>
                     </Route>
                     <ProtectedRoute exact path="/main" component={Main} onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick}
-                        onEditAvatar={handleEditAvatarClick} onCardClick={HandleCardClick} onCardLike={handleCardLike} onCardDelete={handleCardDelete} cards={cards}>
+                        onEditAvatar={handleEditAvatarClick} onCardClick={HandleCardClick} onCardLike={handleCardLike} onCardDelete={handleCardDelete} cards={cards}
+                        loggedIn={loggedIn}>
                     </ProtectedRoute>
                     <Route path="/">
                         {loggedIn ? <Redirect to="/main"/> : <Redirect to="/sign-in"/>}
@@ -166,6 +242,7 @@ function App() {
                 <ImagePopup
                     card={selectedCard}
                     onClose={closeAllPopups}/>
+                <InfoTooltip onClose={closeAllPopups} isOpen={isToooltipOpen} message={message}/>
             </div>
         </div>
         </CurrentUserContext.Provider>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,34 @@
 import React from "react";
 
-function Login() {
+function Login({authorization}) {
+
+    const [valueEmail, setValueEmail] = React.useState('')
+    const [valuePassword, setValuePassword] = React.useState('')
+
+
+
+    function handleChangeEmail(evt) {
+        setValueEmail(evt.target.value)
+    }
+
+    function handleChangePassword(evt) {
+        setValuePassword(evt.target.value)
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault()
+        const email = valueEmail;
+        const password = valuePassword;
+        authorization({password, email})
+
+    }
+
     return (
         <section className="login">
             <h1 className="login__title">Вход</h1>
-            <form className="login__form">
-                <input  className="login__input" placeholder="Email"/>
-                <input  className="login__input" placeholder="Пароль"/>
+            <form onSubmit={handleSubmit} className="login__form">
+                <input value={valueEmail} onChange={handleChangeEmail}  className="login__input" placeholder="Email"/>
+                <input type="password" value={valuePassword} onChange={handleChangePassword}  className="login__input" placeholder="Пароль"/>
                 <button className="login__button-submit">Войти</button>
             </form>
         </section>
